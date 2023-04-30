55913a3a6342e4cdda58347ac07b1892ba2ba624
Federica d'Incalci (Mac) <federica.dincalci@gmail.com>
2023-04-30 15:33:27 +0200

    Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "3139d6f9-b2e5-4beb-853e-daee8ce5f124";
+        private const string _tenantId = "f8cdef31-a31e-4b4a-93e4-5f571e91255a";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+
+            string[] scopes = { "user.read" };
+
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
